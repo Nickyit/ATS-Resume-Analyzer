e6b01696126b011143168ec27e1f8b790be100e1
Nikita Digodiya <nikitadigodiya31@gmail.com>
2026-06-17 13:34:36 +0530

    Update features and make it mobile friendly website

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -43,4 +43,4 @@ def home():
     return render_template("resume.html", data=None)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5001)
